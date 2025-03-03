440a39398a3dd070f24f5b30d030b8d16360a0ef
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-03-03 13:51:18 -0500

    complete

--- W25Week8IntroToMaui/MainPage.xaml.cs
+++ W25Week8IntroToMaui/MainPage.xaml.cs
@@ -1,16 +1,24 @@
-﻿namespace W25Week8IntroToMaui
+﻿using System.Collections.ObjectModel;
+
+namespace W25Week8IntroToMaui
 {
     public partial class MainPage : ContentPage
     {
-        
+        private ObservableCollection<string> tasks = new ObservableCollection<string>();
+
         public MainPage()
         {
             InitializeComponent();
+            lvTasks.ItemsSource = tasks;
         }
 
         private void btnAddTask_Clicked(object sender, EventArgs e)
         {
-
+            if (!string.IsNullOrWhiteSpace(txtTask.Text))
+            {
+                tasks.Add(txtTask.Text);
+                txtTask.Text = string.Empty;
+            }
         }
     }
 
